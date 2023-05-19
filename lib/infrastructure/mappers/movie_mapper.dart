82eaf90d1e07dc01898a3e05a4f5cca93f49f579
jeison <jeison.salazar71@gmail.com>
2023-05-19 15:05:53 -0500

    Fin structura codigo limpio

---
+++ lib/infrastructure/mappers/movie_mapper.dart
@@ -0,0 +1,27 @@
+//la mision  es leer diferentes modelos y crear mi entidad
+
+import 'package:cinemapedia/domain/entities/movie.dart';
+import 'package:cinemapedia/infrastructure/models/moviesdb/movie_moviedb.dart';
+
+class MovieMapper {
+  static Movie movieDBToEntity(MovieMovieDb moviedb) => Movie(
+        adult: moviedb.adult,
+        backdropPath: (moviedb.backdropPath != '')
+            ? 'https://image.tmdb.org/t/p/w500/${moviedb.backdropPath}'
+            : 'https://ih1.redbubble.net/image.533910704.5853/poster,504x498,f8f8f8-pad,600x600,f8f8f8.u3.jpg',
+        genreIds: moviedb.genreIds.map((e) => e.toString()).toList(),
+        id: moviedb.id,
+        originalLanguage: moviedb.originalLanguage,
+        originalTitle: moviedb.originalTitle,
+        overview: moviedb.overview,
+        popularity: moviedb.popularity,
+        posterPath: (moviedb.posterPath != '')
+            ? 'https://image.tmdb.org/t/p/w500/${moviedb.posterPath}'
+            : 'no-poster',
+        releaseDate: moviedb.releaseDate,
+        title: moviedb.title,
+        video: moviedb.video,
+        voteAverage: moviedb.voteAverage,
+        voteCount: moviedb.voteCount,
+      );
+}
--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -1,16 +1,48 @@
+import 'package:cinemapedia/presentation/providers/providers.dart';
 import 'package:flutter/material.dart';
-import 'package:cinemapedia/config/constants/environment.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+//
 class HomeScreen extends StatelessWidget {
   static const name = "home-screen";
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text(Environment.theMovieDbKey),
-      ),
+    return const Scaffold(
+      body: _HomeView(),
+    );
+  }
+}
+
+//se transforma el stateFullwidge en un ConsumerStateFulWidget
+class _HomeView extends ConsumerStatefulWidget {
+  const _HomeView();
+
+  @override
+  _HomeViewState createState() => _HomeViewState();
+}
+
+//Cambiamos el State por ConsumerState
+class _HomeViewState extends ConsumerState<_HomeView> {
+  @override
+  void initState() {
+    super.initState();
+
+    ref.read(nowPlayingMoviesProviders.notifier).loadNextPage();
+  }
+
+  Widget build(BuildContext context) {
+    //
+    final nowPlayingMovies = ref.watch(nowPlayingMoviesProviders);
+    return ListView.builder(
+      itemCount: nowPlayingMovies.length,
+      itemBuilder: (context, index) {
+        final movie = nowPlayingMovies[index];
+        return ListTile(
+          title: Text(movie.title),
+        );
+      },
     );
   }
 }
